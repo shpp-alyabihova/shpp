0a4ba9b1015f9c8cb76811a09fd7a18104d0ccec
alyabihova <sunny4ka@gmail.com>
2016-01-07 22:45:05 +0200

    minor fixes

--- day4-js-in-browser/toDoList/js/script.js
+++ day4-js-in-browser/toDoList/js/script.js
@@ -1,19 +1,30 @@
-var newTask = document.getElementByClassName("taskField");
-var toDoList = document.getElementByClassName("list");
+
 
 function readValue(event){
     if (event.keyCode==13) {
+        var newTask = document.getElementById('newTask');
         var currentTask = newTask.value;
         if (currentTask == "") {
             return false;
         }
-        var taskLabel = document.createElement('label');
-        var textTask = document.createTextNode(currentTask);
-        taskLabel.appendChild(textTask);
-        var taskState = document.createElement('input');
-        taskState.className = 'taskStatus';
-        taskState.type = "checkbox";
-        toDoList.appendChild(taskLabel);
-        taskLabel.appendChild(taskState);
+        addTaskToList(currentTask);
+        newTask.value = "";
     }
 }
+
+function addTaskToList(currentTask){
+    var toDoList = document.getElementById('tasksField');
+    var singleTaskField = document.createElement('div');
+    singleTaskField.className = 'singleTask';
+    toDoList.appendChild(singleTaskField);
+    var taskState = document.createElement('input');
+    taskState.className = 'taskStatus';
+    taskState.type = "checkbox";
+    singleTaskField.appendChild(taskState);
+    var _toDo = document.createElement('span');
+    var textTask = document.createTextNode(currentTask);
+    _toDo.className = 'toDo';
+    _toDo.appendChild(textTask);
+    singleTaskField.appendChild(_toDo);
+}
+
