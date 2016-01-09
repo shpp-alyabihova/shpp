ec2ea4b3abfc6676c851e995823351e025743a44
alyabihova <sunny4ka@gmail.com>
2016-01-09 03:38:06 +0200

    +checked

--- day4-js-in-browser/toDoList/js/script.js
+++ day4-js-in-browser/toDoList/js/script.js
@@ -1,3 +1,4 @@
+var allActiveTasks = 0;
 
 
 function readValue(event){
@@ -12,8 +13,13 @@ function readValue(event){
     }
 }
 
+
+var toDoList = document.getElementById('tasksField');
+
+
 function addTaskToList(currentTask){
-    var toDoList = document.getElementById('tasksField');
+    allActiveTasks++;
+    output.setAttribute('value', allActiveTasks);
     var singleTaskField = document.createElement('div');
     singleTaskField.className = 'singleTask';
     toDoList.appendChild(singleTaskField);
@@ -26,5 +32,120 @@ function addTaskToList(currentTask){
     _toDo.className = 'toDo';
     _toDo.appendChild(textTask);
     singleTaskField.appendChild(_toDo);
+    var closeTask = document.createElement('div');
+    closeTask.className = 'delTask';
+    var closeMark = document.createTextNode(String.fromCharCode(215));
+    closeTask.appendChild(closeMark);
+    singleTaskField.appendChild(closeTask);
 }
+
+
+var output = document.querySelector('.leftTasks');
+
+var delMark = document.querySelector('.delTask');
+if(delMark){
+    alert('Catch');
+    delMark.addEventListener('click', function() {
+        delMark.parentNode.removeChild(delMark);
+    });
+}
+
+//check tasks
+//======================================================================================================================
+var mainCheck = document.getElementById('checkAll');
+mainCheck.addEventListener('click', checkAllPoints);
+
+function checkAllPoints(){
+    var allTasks = document.querySelectorAll('.taskStatus');
+    if(mainCheck.checked == true) {
+        for (var i = 0; i < allTasks.length; i++) {
+            allTasks[i].checked = true;
+        }
+        allActiveTasks = 0;
+        output.setAttribute('value', allActiveTasks);
+    }
+    else{
+        for (var i = 0; i < allTasks.length; i++) {
+            allTasks[i].checked = false;
+        }
+        allActiveTasks = allTasks.length;
+        output.setAttribute('value', allActiveTasks);
+    }
+}
+
+
+toDoList.addEventListener('click', function() {
+    var allTasks = document.querySelectorAll('.taskStatus');
+    var counterActiveTasks = 0;
+    for (var i = 0; i < allTasks.length; i++) {
+        if (allTasks[i].checked == false) {
+            counterActiveTasks++;
+        }
+    }
+    if (counterActiveTasks == 0){
+        mainCheck.checked = true;
+    }
+    else {
+        mainCheck.checked = false;
+    }
+    allActiveTasks = counterActiveTasks;
+    output.setAttribute('value', allActiveTasks);
+});
+
+
+// control menu
+//======================================================================================================================
+var buttonAll = document.querySelector('.allTasks');
+buttonAll.addEventListener('click', function(){
+    var allTasksFields = document.querySelectorAll('.singleTask');
+    for (var i = 0; i < allTasksFields.length; i++) {
+        allTasksFields[i].style.opacity = 1;
+        allTasksFields[i].style.position = 'inherit';
+    }
+});
+
+var buttonActive = document.querySelector('.activeTasks');
+buttonActive.addEventListener('click', function(){
+    var allTasks = document.querySelectorAll('.taskStatus');
+    var allTasksFields = document.querySelectorAll('.singleTask');
+    for (var i = 0; i < allTasks.length; i++) {
+        if(allTasks[i].checked == true) {
+            allTasksFields[i].style.opacity = 0;
+            allTasksFields[i].style.position = 'absolute';
+        }
+        else{
+            allTasksFields[i].style.opacity = 1;
+            allTasksFields[i].style.position = 'inherit';
+        }
+    }
+});
+
+var buttonDone = document.querySelector('.doneTasks');
+buttonDone.addEventListener('click', function(){
+    var allTasks = document.querySelectorAll('.taskStatus');
+    var allTasksFields = document.querySelectorAll('.singleTask');
+    for (var i = 0; i < allTasks.length; i++) {
+        if(allTasks[i].checked == false){
+            allTasksFields[i].style.opacity = 0;
+            allTasksFields[i].style.position = 'absolute';
+        }
+        else{
+            allTasksFields[i].style.opacity = 1;
+            allTasksFields[i].style.position = 'inherit';
+        }
+    }
+});
+
+var buttonClear = document.querySelector('.clearTasks');
+buttonClear.addEventListener('click', function(){
+    var allTasks = document.querySelectorAll('.taskStatus');
+    var allTasksFields = document.querySelectorAll('.singleTask');
+    for (var i = 0; i < allTasks.length; i++) {
+        if(allTasks[i].checked == true) allTasksFields[i].remove();
+    }
+});
+
+
+
+//======================================================================================================================
 
